326b861497d248f7d252242b47bb4146751d8fdf
ReenExe <ipourri@gmail.com>
2016-07-11 19:46:20 +0300

    generate (#6) but move test

---
+++ src/AppBundle/Controller/DeveloperController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DeveloperController extends Controller
+{
+    public function listAction()
+    {
+        return $this->render('AppBundle:Developer:list.html.twig', array(
+            // ...
+        ));
+    }
+
+}
