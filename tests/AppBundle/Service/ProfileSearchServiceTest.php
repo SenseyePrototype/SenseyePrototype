da17962a75bdbd947a83892b9b8836244fab87fd
ReenExe <ipourri@gmail.com>
2016-11-29 19:50:16 +0200

    pass simple elastic test (#15)

--- tests/AppBundle/Service/ProfileSearchServiceTest.php
+++ tests/AppBundle/Service/ProfileSearchServiceTest.php
@@ -2,10 +2,28 @@
 
 namespace Tests\AppBundle\Service;
 
+use Elastica\Document;
+
 class ProfileSearchServiceTest extends AbstractServiceTest
 {
     public function test()
     {
-        $this->container->get('senseye.elasticsearch.client')->getStatus();
+        $client = $this->container->get('senseye.elasticsearch.client');
+
+        $index = $client->getIndex('developer');
+        $type = $index->getType('profile');
+
+        $data = [
+            'title' => 'PHP developer',
+            'description' => 'Develop current project'
+        ];
+
+        $type->addDocument(new Document(1, $data));
+
+        $index->refresh();
+
+        $result = $index->search();
+
+        $this->assertSame($data, $result[0]->getSource());
     }
 }
