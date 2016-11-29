9cd94d88fc430016a50819e257f9a0a5eb69bef2
ReenExe <ipourri@gmail.com>
2016-11-29 19:12:23 +0200

    update routing (#15)

--- tests/AppBundle/Controller/DeveloperControllerTest.php
+++ tests/AppBundle/Controller/DeveloperControllerTest.php
@@ -11,6 +11,9 @@ class DeveloperControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/developers');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Welcome to Developer:list page', $crawler->filter('body h1')->text());
     }
 
 }
