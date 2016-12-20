5903e05aee5c2d31729c2ac2f15b0332cf713010
ReenExe <ipourri@gmail.com>
2016-12-20 23:57:27 +0200

    pass tests (#21)

--- src/AppBundle/Component/ProfileSearchResponse.php
+++ src/AppBundle/Component/ProfileSearchResponse.php
@@ -2,7 +2,20 @@
 
 namespace AppBundle\Component;
 
+use ReenExeCubeTime\LightPaginator\Pager;
+
 class ProfileSearchResponse
 {
+    /**
+     * @var Pager
+     */
+    private $pager;
 
+    /**
+     * @return Pager
+     */
+    public function getPager()
+    {
+        return $this->pager;
+    }
 }
