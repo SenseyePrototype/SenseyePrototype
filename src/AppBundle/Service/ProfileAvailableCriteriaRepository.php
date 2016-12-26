30fe52027fbced24199425ab03c4d563dc6d47e7
ReenExe <ipourri@gmail.com>
2016-12-26 23:01:25 +0200

    default available criteria (#32)

--- src/AppBundle/Service/ProfileAvailableCriteriaRepository.php
+++ src/AppBundle/Service/ProfileAvailableCriteriaRepository.php
@@ -3,11 +3,40 @@
 namespace AppBundle\Service;
 
 use AppBundle\Component\ProfileAvailableCriteria;
+use Elastica\Document;
 
 class ProfileAvailableCriteriaRepository
 {
+    /**
+     * @var string
+     */
+    private $id = 'available';
+
+    /**
+     * @var DeveloperIndexService
+     */
+    private $indexService;
+
+    /**
+     * ProfileAvailableCriteriaRepository constructor.
+     * @param DeveloperIndexService $indexService
+     */
+    public function __construct(DeveloperIndexService $indexService)
+    {
+        $this->indexService = $indexService;
+    }
+
     public function get()
     {
         return new ProfileAvailableCriteria([], [], []);
+    }
+
+    public function store(array $available)
+    {
+        $type = $this->indexService->getFilter();
+
+        $type->addDocument(new Document($this->id, $available));
+
+        $type->getIndex()->refresh();
     }
 }
--- tests/AppBundle/Service/ProfileAvailableCriteriaTest.php
+++ tests/AppBundle/Service/ProfileAvailableCriteriaTest.php
@@ -11,5 +11,21 @@ class ProfileAvailableCriteriaTest extends TestCase
         $index = $searchable->getIndex();
 
         $this->clearIndex($index);
+
+        $source = [
+            'multi' => [],
+            'must' => [],
+            'range' => [],
+        ];
+
+        $repository = $this->container->get('senseye.profile.available.criteria.repository');
+
+        $repository->store($source);
+
+        $criteria = $repository->get();
+
+        $this->assertSame($source['multi'], $criteria->getMultiMap());
+        $this->assertSame($source['must'], $criteria->getMustMap());
+        $this->assertSame($source['range'], $criteria->getRangeMap());
     }
 }
