058fed26b217e5cdbd6678cf9b0ea2197c9c84d1
ReenExe <ipourri@gmail.com>
2016-12-31 02:41:00 +0200

    add ApiDoc for action (#45)

--- src/AppBundle/Controller/ApiDeveloperController.php
+++ src/AppBundle/Controller/ApiDeveloperController.php
@@ -5,9 +5,23 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class ApiDeveloperController extends Controller
 {
+    /**
+     * @ApiDoc(
+     *      description="Developers count by criteria",
+     *      parameters={
+     *          {"name": "skills", "dataType": "string", "required": false, "description": "aliases separated my comma"},
+     *          {"name": "cities", "dataType": "string", "required": false, "description": "aliases separated my comma"},
+     *          {"name": "experience", "dataType": "string", "required": false, "description": "range from-to"},
+     *          {"name": "salary", "dataType": "string", "required": false, "description": "range from-to"},
+     *      }
+     * )
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function countAction(Request $request)
     {
         $available = $this->get('senseye.profile.available.criteria.repository')->get();
