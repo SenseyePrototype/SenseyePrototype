c12db8e7efe3f1a00f2de182eddeaa47199906fd
ReenExe <ipourri@gmail.com>
2017-01-22 22:03:16 +0200

    sitemap (#50)

--- src/AppBundle/Command/GenerateSitemapCommand.php
+++ src/AppBundle/Command/GenerateSitemapCommand.php
@@ -15,6 +15,35 @@ class GenerateSitemapCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Command result.');
+        $root = dirname($this->getContainer()->getParameter('kernel.root_dir'));
+
+        $xml = new \DOMDocument('1.0', 'UTF-8');
+
+        $setXml = $xml->createElement('urlset');
+
+        $setXml->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+        $available = $this->getContainer()->get('senseye.profile.available.criteria.repository')->get();
+
+        $urls = [
+            'http://senseye.com.ua/developers'
+        ];
+
+        $cities = $available->getMultiMap()['cities'];
+        foreach ($cities as $city) {
+            $urls[] = "http://senseye.com.ua/developers/{$city['alias']}";
+        }
+
+        foreach ($urls as $url) {
+            $urlXml = $xml->createElement('url');
+            $locXml = $xml->createElement('loc', $url);
+            $urlXml->appendChild($locXml);
+            $setXml->appendChild($urlXml);
+        }
+
+        $xml->appendChild($setXml);
+        $xml->formatOutput = true;
+        $xml->preserveWhiteSpace = false;
+        $xml->save($root . '/web/sitemap.xml');
     }
 }
