49ab7844b62f03388e62f7b559ac14b55c69b716
ReenExe <ipourri@gmail.com>
2017-01-28 00:43:14 +0200

    testing create developer profile (#69)

--- src/AppBundle/Controller/IntroController.php
+++ src/AppBundle/Controller/IntroController.php
@@ -2,12 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\DeveloperProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IntroController extends Controller
 {
     public function indexAction()
     {
+        $developerProfile = new DeveloperProfile();
+
+        $developerProfile
+            ->setUser($this->getUser())
+            ->setTitle('Backend developer')
+            ->setSalary(5000)
+            ->setExperience(7)
+        ;
+
+        $this->getDoctrine()->getManager()->persist($developerProfile);
+        $this->getDoctrine()->getManager()->flush($developerProfile);
+
         return $this->render('AppBundle:Intro:index.html.twig');
     }
 }
