4c6b10bd7a6c73864bde85c2f5a622ee584fe141
ReenExe <ipourri@gmail.com>
2017-01-29 23:59:49 +0200

    simple auth modal

--- web/developers.js
+++ web/developers.js
@@ -125,4 +125,12 @@ $(document).ready(function () {
             search();
         }
     });
+
+    $('.js-auth-open').on('click', function(){
+        $('body').addClass('modal-open');
+    });
+
+    $('.modal-popup .close, .modal-popup .modal-bg').on('click', function(){
+        $('body').removeClass('modal-open');
+    });
 });
