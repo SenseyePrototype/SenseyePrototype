c8da95a9da982a89f490d0b02f7dd7d9279fc7f2
ReenExe <ipourri@gmail.com>
2017-02-19 00:08:28 +0200

    cleanup

--- frontend/skills.js
+++ frontend/skills.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 import material from 'materialize-css';
 
+const skills = global.skills;
+
 $(global.document).ready(function () {
-    var data = {};
-    for (var index in global.skills) {
-        data[global.skills[index]] = null;
+    let data = {};
+    for (let index in skills) {
+        data[skills[index]] = null;
     }
 
     $('input.autocomplete').autocomplete({
