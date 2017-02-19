cdd8d31b5184785867a45382f33b9b7ed779f2a0
ReenExe <ipourri@gmail.com>
2017-02-19 02:59:50 +0200

    default JavaScript (#96)

--- frontend/skills.js
+++ frontend/skills.js
@@ -5,6 +5,28 @@ import api from 'senseye/api';
 const skills = global.skills;
 
 $(global.document).ready(function () {
+    let $skills = $('.js-skill-list');
+    let $complete = $('.js-skill-autocomplete');
+
+    function renderSkill(name) {
+        const template = $('#js-skill-template').html();
+
+        let $skill = $(template);
+
+        $skill.find('.js-name').html(name);
+
+        $skills.prepend($skill);
+    }
+
+    function addSkill(name) {
+        if (skills.hasOwnProperty(name)) {
+            api.addSkill(skills[name], function () {
+                renderSkill(name);
+                $complete.val('');
+            });
+        }
+    }
+
     let data = {};
     for (let name in skills) {
         if (skills.hasOwnProperty(name)) {
@@ -12,15 +34,27 @@ $(global.document).ready(function () {
         }
     }
 
-    let $autocomplete = $('.js-skill-autocomplete');
+    $complete.focus();
 
-    $autocomplete.autocomplete({
+    $complete.autocomplete({
         data: data,
         limit: 20,
-        onAutocomplete: function (name) {
-            if (skills.hasOwnProperty(name)) {
-                api.addSkill(skills[name]);
-            }
-        }
+        onAutocomplete: addSkill
+    });
+
+    $('.js-add-skill').click(function () {
+        addSkill($complete.val());
+    });
+
+    $skills.on('click', '.js-delete', function () {
+        $(this).closest('.js-skill').remove();
+    });
+
+    $skills.on('click', '.point', function () {
+        var $point = $(this);
+
+        $point.nextAll().removeClass('active');
+        $point.prevAll().addClass('active');
+        $point.addClass('active');
     });
 });
