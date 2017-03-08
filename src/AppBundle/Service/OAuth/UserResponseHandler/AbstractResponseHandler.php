372f599c96f34f759a9aed4a411e8e030c9fce8a
ReenExe <ipourri@gmail.com>
2017-03-08 13:52:49 +0200

    mark as TODO (#111)

--- src/AppBundle/Service/OAuth/UserResponseHandler/AbstractResponseHandler.php
+++ src/AppBundle/Service/OAuth/UserResponseHandler/AbstractResponseHandler.php
@@ -21,7 +21,12 @@ abstract class AbstractResponseHandler implements ResponseHandlerInterface
      */
     private $socialNameCodeMap = [
         'github' => SocialProfile::SOCIAL_CODE_GITHUB,
+
+        /**
+         * TODO fetch email
+         */
         'bitbucket' => SocialProfile::SOCIAL_CODE_BITBUCKET,
+
         'facebook' => SocialProfile::SOCIAL_CODE_FACEBOOK,
         'linkedin' => SocialProfile::SOCIAL_CODE_LINKEDIN,
         'google' => SocialProfile::SOCIAL_CODE_GOOGLE,
